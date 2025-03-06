1ffc11f356cc4645eb2d48f2541328a3b9230c3d
Luka Povroznik <l.povroznik@asbis.com>
2025-03-06 17:32:57 +0100

    minor tweaks

--- main.py
+++ main.py
@@ -17,8 +17,23 @@ DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 DATA = "customer_segmentation_data"
 
 def data_normalisation():
-    pass
+    for file in os.listdir(DATA):
+        print(file)
+        if "Test" in file:
+            file_path = os.path.join(DATA, file)
+            df_test = pd.read_csv(file_path)
+            print(f"Loaded {file}")
+
+            #Mapping numerical values to text fields
+            df_test["Gender"] = df_test["Gender"].map({"Male" : 0, "Female" : 1})
+            df_test["Ever_Married"] = df_test["Ever_Married"].map({"No" : 0, "Yes" : 1})
+            df_test["Graduated"] = df_test["Graduated"].map({"No" : 0, "Yes" : 1})
+            df_test = pd.get_dummies(df_test, columns=["Profession", "Spending_Score"])
+
+            print(df_test.head())
+            df_test.to_csv(os.path.join(DATA, "df_test_normalised.csv"))
 
 def customer_segmentation(df):
     pass
 
+data_normalisation()
